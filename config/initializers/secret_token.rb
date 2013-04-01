b29eb319609f79fcadc2f86164bfaef355470f04
Mike Begley <spam@hell.org>
2013-04-01 15:41:26 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ph::Application.config.secret_token = 'e0aaa570a004d1c005021482e2403dc0bc21b44728d56111d50d4fae61cdf4b4b22a38a2baf4e08a3d00748c23297a81bb12d260fff65ea8c14842e9ec87d48f'
